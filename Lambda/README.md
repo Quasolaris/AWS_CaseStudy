dd91555701429020868ef2b514a30623138e3cc4
Marcel Soltermann <marcel.soltermann@students.fhnw.ch>
2022-12-31 12:41:21 +0100

    [ADD] code for the lambda function added

---
+++ Lambda/README.md
@@ -0,0 +1,1 @@
+# Source Code für die Lambda Funktion
---
+++ Lambda/src/main/java/ch/fhnw/pcls/InputLengthException.java
@@ -0,0 +1,9 @@
+package main.java.ch.fhnw.pcls;
+
+import java.lang.RuntimeException;
+
+public class InputLengthException extends RuntimeException { 
+    public InputLengthException(String errorMessage) {
+        super(errorMessage);
+    }
+}
