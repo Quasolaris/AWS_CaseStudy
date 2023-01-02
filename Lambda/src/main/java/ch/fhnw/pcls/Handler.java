c17e5033daab233a0cb06816de3444b0d33158d2
Marcel Soltermann <marcel.soltermann@students.fhnw.ch>
2023-01-02 20:52:29 +0100

    [CHG] Back to previous version but added some CORS Headers

--- Lambda/src/main/java/ch/fhnw/pcls/Handler.java
+++ Lambda/src/main/java/ch/fhnw/pcls/Handler.java
@@ -2,37 +2,26 @@ package ch.fhnw.pcls;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
-import java.util.HashMap;
 import java.util.List;
 
-// Handler value: example.HandlerDivide
-public class Handler implements RequestHandler<List<Integer>, APIGatewayProxyResponseEvent>{
+public class Handler implements RequestHandler<List<Integer>, Integer>{
   Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
   @Override
-  public APIGatewayProxyResponseEvent handleRequest(List<Integer> event, Context context)
+  public Integer handleRequest(List<Integer> event, Context context)
   {
     LambdaLogger logger = context.getLogger();
-    APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-    response.setIsBase64Encoded(false);
-    response.setStatusCode(200);
-    HashMap<String, String> headers = new HashMap<String, String>();
-    headers.put("Content-Type", "text/html");
-    headers.put("Access-Control-Allow-Headers", "Content-Type");
-    headers.put("Access-Control-Allow-Origin", "*");
-    headers.put("Access-Control-Allow-Methods", "OPTIONS,POST,GET");
-    response.setHeaders(headers);
+    // process event
+    if ( event.size() < 2 )
+    {
+      throw new InputLengthException("Input must be an array that contains 2 numbers.");
+    }
+    int numerator = event.get(0);
+    int denominator = event.get(1);
     logger.log("EVENT: " + gson.toJson(event));
     logger.log("EVENT TYPE: " + event.getClass().toString());
-    response.setBody("HelloWorld");
-    
-    return response;
+    return numerator/denominator;
   }
 }
